262452f33c13bc45afd6b8804bf959c84949ee84
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-09-03 14:41:02 +0600

    restarting project

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './Routes/Routes.jsx'
-import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
-      <RouterProvider router={router} />
-    </HelmetProvider>
+    <App />
   </StrictMode>,
 )
