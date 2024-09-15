51797a415f5ff98e0d22256334f7f3ce25487938
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-09-15 14:02:17 +0600

    my added pets update and adopted button functionality added

--- src/Pages/MyAddedPets/MyAddedPets.jsx
+++ src/Pages/MyAddedPets/MyAddedPets.jsx
@@ -1,62 +1,115 @@
 import SectionTitleWithBGPhoto from "@/Components/SectionTitleWithBGPhoto/SectionTitleWithBGPhoto";
 import useAuth from "@/hooks/useAuth";
 import useAxiosSecure from "@/hooks/useAxiosSecure";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { FaSortDown, FaSortUp } from "react-icons/fa6";
 import { usePagination, useSortBy, useTable } from "react-table";
 import "./MyAddedPets.css";
 import { Button } from "@/Components/ui/button";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 
-const columns = [
-    {
-        Header: "Serial Number",
-        accessor: "id",
-        Cell: (value) => (  
-            <span>{parseInt(value.cell.row.id) + 1}</span>
-        )
-    },
-    {
-        Header: "Pet Name",
-        accessor: "name",
-    },
-    {
-        Header: "Category",
-        accessor: "category",
-    },
-    {
-        Header: "Pet Image",
-        accessor: "image",
-        Cell: ({value}) => (
-            <div>
-                <img src={value} alt="" className="w-12 h-12 rounded-lg" />
-            </div>
-        ),
-    },
-    {
-        Header: "Adoption Status",
-        accessor: "adopted",
-        Cell: ({ value }) => (value ? "Adopted" : "Not Adopted"),
-    },
-    {
-        Header: "Actions",
-        accessor: "_id",
-        Cell: (value) => (
-            <div className="flex gap-2">
-                <Link to={`/dashboard/PetUpdate/${value.cell.value}`} className="bg-orange-400 text-white px-3 py-2 hover:bg-orange-300 rounded-lg">Update</Link>
-                <button className="bg-red-600 text-white px-3 py-2 hover:bg-red-300 rounded-lg">Delete</button>
-                <button className="bg-green-400 text-white px-3 py-2 hover:bg-green-300 rounded-lg">Adopted</button>
-            </div>
-        ),
-    },
-]
+
 
 const MyAddedPets = () => {
     const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
     const [data, setData] = useState([]);
+
+    const handleDelete = useCallback((id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                const res = await axiosSecure.delete(`/my-added-pets/${id}`);
+                if (res.data.deletedCount > 0) {
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    });
+                    setData(previousData => previousData.filter(pet => pet._id !== id));
+                }
+            }
+        });
+    }, [axiosSecure]);
+
+
+    const handleAdopted = useCallback((id) => {
+        Swal.fire({
+            title: "Mark as Adopted?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes!"
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                const res = await axiosSecure.patch(`/my-added-pets/${id}`);
+                if (res.data.modifiedCount > 0) {
+                    Swal.fire({
+                        title: "Adopted!",
+                        text: "Your pet has been adopted.",
+                        icon: "success"
+                    });
+                    setData(previousData => previousData.filter(pet => pet._id !== id));
+                }
+            }
+        });
+    }, [axiosSecure]);
+
+    const columns = useMemo(() => [
+        {
+            Header: "Serial Number",
+            accessor: "id",
+            Cell: (value) => (
+                <span>{parseInt(value.cell.row.id) + 1}</span>
+            )
+        },
+        {
+            Header: "Pet Name",
+            accessor: "name",
+        },
+        {
+            Header: "Category",
+            accessor: "category",
+        },
+        {
+            Header: "Pet Image",
+            accessor: "image",
+            Cell: ({ value }) => (
+                <div>
+                    <img src={value} alt="" className="w-12 h-12 rounded-lg" />
+                </div>
+            ),
+        },
+        {
+            Header: "Adoption Status",
+            accessor: "adopted",
+            Cell: ({ value }) => (value ? <p className="text-green-600 bg-green-200 border-green-600 text-center py-2 rounded-md">Adopted</p> : <p className="text-orange-600 bg-orange-200 border-orange-600 text-center py-2 rounded-md">Not Adopted</p>),
+        },
+        {
+            Header: "Actions",
+            accessor: "_id",
+            Cell: (value) => (
+                <div className="flex gap-2">
+                    <Link to={`/dashboard/PetUpdate/${value.cell.value}`} className="bg-orange-400 text-white px-3 py-2 hover:bg-orange-300 rounded-lg">Update</Link>
+                    <button onClick={() => handleDelete(value.cell.value)} className="bg-red-600 text-white px-3 py-2 hover:bg-red-300 rounded-lg">Delete</button>
+                    <button onClick={() => handleAdopted(value.cell.value)} className="bg-green-400 text-white px-3 py-2 hover:bg-green-300 rounded-lg">Adopt</button>
+                </div>
+            ),
+        },
+    ], [handleAdopted, handleDelete]);
+
+
 
     useEffect(() => {
         axiosSecure.get(`/my-added-pets/${user?.email}`)
@@ -70,6 +123,8 @@ const MyAddedPets = () => {
     },
         useSortBy,
         usePagination)
+
+
     return (
         <div>
             <Helmet>
